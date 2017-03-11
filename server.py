48de920c5a30d3d3b960d7d6f11b9ad4a780b62f
a-staab <astaab@gmail.com>
2017-03-11 22:14:38 +0000

    Updated routes and styling for landing page

--- server.py
+++ server.py
@@ -9,8 +9,30 @@ app = Flask(__name__)
 app.secret_key = "ABC"
 
 
-# YOUR ROUTES GO HERE
+@app.route('/')
+def show_landing_page():
+    """Return landing page with link to application form."""
 
+    return render_template("index.html")
+
+
+@app.route('/application-form')
+def display_form():
+    """Displays job application form."""
+
+    return render_template("application-form.html")
+
+
+@app.route('/application-success', methods=["POST"])
+def show_confirmation():
+    """Show confirmation page repeating applicant's name, minimum salary, and
+    the role for which they're applying.
+    """
+
+# job_app = request.args.get("")
+#         = request.args.get("")
+
+# return 
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
